03505f032c594d67e7bc37381b6379d0dbfb4007
Linus Boehm <linusboehm@gmail.com>
2024-04-01 12:33:25 +0000

    feat: harpoon: don't open in aerial

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,34 +1,56 @@
+local misc_util = require("util.misc")
+
 return {
   "ThePrimeagen/harpoon",
   event = "VeryLazy",
-  keys = {
-    { "<leader>H", false },
-    { "<leader>h", false },
-    {
-      "<leader>ha",
-      function()
-        require("harpoon"):list():append()
-      end,
-      desc = "Harpoon File",
-    },
-    {
-      "<leader>hh",
-      function()
-        require("harpoon").ui:toggle_quick_menu(harpoon:list())
-      end,
-      desc = "Harpoon Quick Menu",
-    },
-    {
-      "<leader>hp",
-      function()
-        require("harpoon"):list():prev()
-      end,
-    },
-    {
-      "<leader>hn",
-      function()
-        require("harpoon"):list():next()
-      end,
-    },
-  },
+
+  keys = function()
+    local harpoon = require("harpoon")
+    local keys = {
+      { "<leader>H", false },
+      { "<leader>h", false },
+      {
+        "<leader>ha",
+        function()
+          harpoon:list():append()
+        end,
+        desc = "Harpoon File",
+      },
+      {
+        "<leader>hh",
+        function()
+          harpoon.ui:toggle_quick_menu(harpoon:list())
+        end,
+        desc = "Harpoon Quick Menu",
+      },
+      {
+        "<leader>hp",
+        function()
+          harpoon:list():prev()
+        end,
+        desc = "Harpoon previous",
+      },
+      {
+        "<leader>hn",
+        function()
+          harpoon:list():next()
+        end,
+        desc = "Harpoon next",
+      },
+    }
+
+    for i = 1, 5 do
+      table.insert(keys, {
+        "<leader>" .. i,
+        function()
+          if vim.bo.filetype == "aerial" then
+            vim.cmd([[wincmd p]])
+          end
+          harpoon:list():select(i)
+        end,
+        desc = "Harpoon to File " .. i,
+      })
+    end
+    return keys
+  end,
 }
