7141a51e86fb8b2de919b6b6bce66cb829bf863e
lboehm <lboehm@eglp.com>
2024-04-22 10:03:28 -0400

    feat: format git hunks using conform

--- lua/plugins/conform_nvim.lua
+++ lua/plugins/conform_nvim.lua
@@ -2,6 +2,9 @@ local prettier = { "prettier", "prettierd" }
 return {
   {
     "stevearc/conform.nvim",
+    dependencies = {
+      { "lewis6991/gitsigns.nvim", lazy = true },
+    },
     opts = {
       formatters_by_ft = {
         ["cmake"] = { "cmake_format" },
@@ -29,6 +32,31 @@ return {
         prettier_gitcommit = {
           command = "format_git.sh",
         },
+      },
+    },
+    keys = {
+      {
+        "<leader>ch",
+        function()
+          local ignore_filetypes = { "lua" }
+          if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
+            vim.notify("range formatting for " .. vim.bo.filetype .. " not working properly.")
+            return
+          end
+          local hunks = require("gitsigns").get_hunks()
+          local format = require("conform").format
+          for _, hunk in pairs(hunks) do
+            if hunk ~= nil then
+              local start = hunk.added.start
+              local last = start + hunk.added.count
+              -- nvim_buf_get_lines uses zero-based indexing -> subtract from last
+              local last_hunk_line = vim.api.nvim_buf_get_lines(0, last - 2, last - 1, true)[1]
+              local range = { start = { start, 0 }, ["end"] = { last - 1, last_hunk_line:len() } }
+              format({ range = range })
+            end
+          end
+        end,
+        desc = "format git hunks",
       },
     },
   },
