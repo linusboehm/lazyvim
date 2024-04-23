9825161b2167e5dccbf37a49d20de73f34211896
Linus Boehm <linusboehm@gmail.com>
2024-04-23 14:53:48 +0000

    feat: don't block on hunk formatting

--- lua/plugins/conform_nvim.lua
+++ lua/plugins/conform_nvim.lua
@@ -55,18 +55,39 @@ return {
             vim.notify("range formatting for " .. vim.bo.filetype .. " not working properly.")
             return
           end
+
           local hunks = require("gitsigns").get_hunks()
+          if hunks == nil then
+            return
+          end
+
           local format = require("conform").format
-          for _, hunk in pairs(hunks) do
-            if hunk ~= nil then
+
+          local function format_range()
+            if next(hunks) == nil then
+              vim.notify("done formatting git hunks", "info", { title = "formatting" })
+              return
+            end
+            local hunk = nil
+            while next(hunks) ~= nil and (hunk == nil or hunk.type == "delete") do
+              hunk = table.remove(hunks)
+            end
+
+            if hunk ~= nil and hunk.type ~= "delete" then
               local start = hunk.added.start
               local last = start + hunk.added.count
               -- nvim_buf_get_lines uses zero-based indexing -> subtract from last
               local last_hunk_line = vim.api.nvim_buf_get_lines(0, last - 2, last - 1, true)[1]
               local range = { start = { start, 0 }, ["end"] = { last - 1, last_hunk_line:len() } }
-              format({ range = range })
+              format({ range = range, async = true }, function()
+                vim.defer_fn(function()
+                  format_range()
+                end, 1)
+              end)
             end
           end
+
+          format_range()
         end,
         desc = "format git hunks",
       },
