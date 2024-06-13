69e9b1b207c50120b04204b788da8499c999d599
Linus Boehm <linusboehm@gmail.com>
2024-06-13 01:43:43 +0000

    fix: disable indent-blankline (was slow)

--- lua/plugins/extra.lua
+++ lua/plugins/extra.lua
@@ -3,6 +3,7 @@ local misc = require("util.misc")
 return {
   {
     "lukas-reineke/indent-blankline.nvim",
+    enabled = false,
     main = "ibl",
     commit = "29be0919b91fb59eca9e90690d76014233392bef",
   },
