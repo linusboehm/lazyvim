d588e938fd26baf0abcdb9db67f7aeacd3eb56f2
Linus Boehm <lboehm@core-dev6.eglp.com>
2024-10-30 23:20:19 +0000

    feat: add db example

--- lua/plugins/vim_dadbod_ui.lua
+++ lua/plugins/vim_dadbod_ui.lua
@@ -10,8 +10,15 @@ return {
     "DBUIAddConnection",
     "DBUIFindBuffer",
   },
-  init = function()
+  config = function()
     vim.g.db_ui_use_nerd_fonts = 1
+    vim.g.db_ui_execute_on_save = 0
+    vim.g.dbs = {
+      -- {
+      --   name = "some_name",
+      --   url = "postgres://user:" .. get_password() .. "@host:port/dbname",
+      -- },
+    }
   end,
   keys = { { mode = { "n" }, "<leader>dd", "<cmd>DBUIToggle<cr>", { desc = "DB" } } },
 }
