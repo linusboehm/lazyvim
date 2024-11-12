901edca8103d53fd37daa0f1940b46300dca4e93
Linus Boehm <linusboehm@gmail.com>
2024-11-11 20:29:17 -0500

    fix: escape special characters for git root

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -46,10 +46,13 @@ map({ "n", "v", "i", "t" }, "<C-Right>", "<cmd>vertical resize +10<cr>", { desc 
 map("n", "<leader>pf", function()
   local path = vim.api.nvim_buf_get_name(0)
   local git_root = Snacks.git.get_root()
+  -- Escape special characters in the leading substring
+  local escaped_git_root = git_root:gsub("([%-%.%+%[%]%(%)%$%^%%%?%*])", "%%%1")
+  Snacks.notify.info(git_root)
   local row, _ = unpack(vim.api.nvim_win_get_cursor(0))
-  path = path:gsub(git_root .. "/", "")
-  CoreUtil.info(path, { title = "current file name" })
-  vim.fn.setreg("+", path .. ":" .. row)
+  short_path = path:gsub(escaped_git_root .. "/", "")
+  CoreUtil.info(short_path, { title = "current file name" })
+  vim.fn.setreg("+", short_path .. ":" .. row)
 end, { desc = "print current filename" })
 
 map("n", "gL", function()
