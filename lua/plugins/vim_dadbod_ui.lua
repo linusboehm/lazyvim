9e80733a5c1f0c43b325e659934d34e2f855dbaa
Linus Boehm <linusboehm@gmail.com>
2024-12-03 14:53:42 +0000

    feat: handle DB credentials outside of nvim

--- lua/plugins/vim_dadbod_ui.lua
+++ lua/plugins/vim_dadbod_ui.lua
@@ -1,3 +1,25 @@
+local function read_json_file(file_path)
+  local lines = vim.fn.readfile(file_path)
+  local content = table.concat(lines, "\n")
+  return vim.fn.json_decode(content)
+end
+
+local function get_db_entries_from_dir(directory)
+  local file_list = vim.fn.globpath(directory, "*", false, true)
+  local entries = {}
+  for _, file_path in ipairs(file_list) do
+    local data = read_json_file(file_path)
+    if data then
+      local entry = {
+        name = vim.fn.fnamemodify(file_path, ":t"),
+        url = ("postgres://%s:%s@%s:%s/%s"):format(data.user, data.password, data.host, data.port, data.dbname),
+      }
+      table.insert(entries, entry)
+    end
+  end
+  return entries
+end
+
 return {
   "kristijanhusak/vim-dadbod-ui",
   dependencies = {
@@ -5,8 +27,6 @@ return {
     { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
   },
   init = function()
-    -- local data_path = vim.fn.stdpath("data")
-
     vim.g.db_ui_auto_execute_table_helpers = 1
     -- vim.g.db_ui_save_location = data_path .. "/dadbod_ui"
     vim.g.db_ui_show_database_icon = true
@@ -21,15 +41,9 @@ return {
     vim.g.db_ui_execute_on_save = false
   end,
   config = function()
-    -- vim.g.db_ui_use_nerd_fonts = 1
-    -- vim.g.db_ui_execute_on_save = 0
-    -- vim.g.db_ui_show_database_icon = 1
-    vim.g.dbs = {
-      -- {
-      --   name = "some_name",
-      --   url = "postgres://user:" .. get_password() .. "@host:port/dbname",
-      -- },
-    }
+    local home_dir = vim.fn.expand("~")
+    local directory = home_dir .. "/creds/db"
+    vim.g.dbs = get_db_entries_from_dir(directory)
   end,
   keys = { { mode = { "n" }, "<leader>dd", "<cmd>DBUIToggle<cr>", { desc = "DB" } } },
 }
