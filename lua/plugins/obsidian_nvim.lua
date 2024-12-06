1b0a855431b7be5cfe4d897d35549853e0c7aa17
Linus Boehm <linusboehm@gmail.com>
2024-12-06 16:27:30 +0000

    fix: disable some checkbox types

--- lua/plugins/obsidian_nvim.lua
+++ lua/plugins/obsidian_nvim.lua
@@ -51,6 +51,21 @@ return {
         opts = { buffer = true, expr = true },
       },
     },
+    ui = {
+      checkboxes = {
+        -- NOTE: the 'char' value has to be a single character, and the highlight groups are defined below.
+        [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
+        ["X"] = { char = "", hl_group = "ObsidianDone" },
+        -- [">"] = { char = "", hl_group = "ObsidianRightArrow" },
+        -- ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
+        -- ["!"] = { char = "", hl_group = "ObsidianImportant" },
+        -- -- Replace the above with this if you don't have a patched font:
+        -- -- [" "] = { char = "☐", hl_group = "ObsidianTodo" },
+        -- -- ["x"] = { char = "✔", hl_group = "ObsidianDone" },
+        --
+        -- -- You can also add more custom ones...
+      },
+    },
     new_notes_location = "notes_subdir",
 
     -- Optional, customize how note IDs are generated given an optional title.
