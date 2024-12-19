f234a8c4665bf297269a4df8d4abac038838e637
Linus Boehm <linusboehm@gmail.com>
2024-12-19 13:55:40 +0000

    fix(blink): fix keyboard shortcuts

--- lua/plugins/blink_cmp.lua
+++ lua/plugins/blink_cmp.lua
@@ -4,14 +4,27 @@ return {
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
+      completion = {
+        list = {
+          max_items = 50,
+          selection = "manual",
+        },
+        -- accept = { auto_brackets = { enabled = true } },
+        menu = {
+          draw = {
+            treesitter = { "lsp" },
+          },
+        },
+      },
+      sources = {
+        providers = {
+          dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink", score_offset = 3 },
+        },
+      },
       keymap = {
         preset = "enter",
-        ["<Tab>"] = {
-          "select_next",
-          "snippet_forward",
-          "fallback",
-        },
-        ["<S-Tab>"] = { "select_prev", "snippet_backward" },
+        ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+        ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
       },
     },
   },
