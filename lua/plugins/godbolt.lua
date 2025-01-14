b5765f507830b738eb4d40e64290d7b898c3c614
Linus Boehm <linusboehm@gmail.com>
2025-01-14 20:37:02 +0000

    feat(godbolt): add visual shortcuts

--- lua/plugins/godbolt.lua
+++ lua/plugins/godbolt.lua
@@ -23,6 +23,7 @@ return {
           vim.cmd("Godbolt")
         end,
         desc = "Godbolt/CompilerExplorer",
+        mode = { "n" },
       },
       {
         "<leader>cG",
@@ -30,6 +31,29 @@ return {
           vim.cmd("GodboltCompiler telescope")
         end,
         desc = "Godbolt/CompilerExplorer telescope",
+        mode = { "n" },
+      },
+      {
+        "<leader>cg",
+        function()
+          -- Get the start and end of the visual selection
+          local start_line = vim.fn.line("'<")
+          local end_line = vim.fn.line("'>")
+
+          -- Run the Godbolt command on the selected lines
+          vim.api.nvim_command(start_line .. "," .. end_line .. "Godbolt")
+          -- vim.cmd("Godbolt")
+        end,
+        desc = "Godbolt/CompilerExplorer",
+        mode = { "v" },
+      },
+      {
+        "<leader>cG",
+        function()
+          vim.cmd("'<,'>GodboltCompiler telescope")
+        end,
+        desc = "Godbolt/CompilerExplorer telescope",
+        mode = { "v" },
       },
     },
   },
