bf31c31b9cec6dce757f4169cdd90c184a0ee1ae
Linus Boehm <linusboehm@gmail.com>
2025-02-26 17:44:42 +0000

    fix(oil): update settings

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,8 +1,24 @@
 return {
   "stevearc/oil.nvim",
-  config = function()
-    local oil = require("oil")
-    oil.setup()
-    vim.keymap.set("n", "-", oil.toggle_float, {})
-  end,
+  opts = {
+    float = {
+      -- max_width and max_height can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+      max_width = 0.8,
+      max_height = 60,
+    },
+    view_options = {
+      -- Show files and directories that start with "."
+      show_hidden = true,
+    },
+  },
+  keys = {
+    {
+      "-",
+      function()
+        require("oil").toggle_float()
+      end,
+      desc = "Open parent directory",
+      mode = "n",
+    },
+  },
 }
