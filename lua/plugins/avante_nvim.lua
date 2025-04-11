71351d30033afd0b0e95c920a6a7d26f3767ed23
Linus Boehm <linusboehm@gmail.com>
2025-04-11 16:10:34 -0400

    feat: enable avante

--- lua/plugins/avante_nvim.lua
+++ lua/plugins/avante_nvim.lua
@@ -1,33 +1,23 @@
 return {
   "yetone/avante.nvim",
-  enabled = false,
   event = "VeryLazy",
-  lazy = false,
-  version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+  version = false,
   opts = {
-    -- add any opts here
-    -- for example
+    file_selector = "snacks",
     provider = "copilot",
-    openai = {
-      endpoint = "https://api.openai.com/v1",
-      model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
-      timeout = 30000, -- timeout in milliseconds
-      temperature = 0, -- adjust if needed
-      max_tokens = 4096,
-      reasoning_effort = "high" -- only supported for "o" models
+    copilot = {
+      model = "claude-3.7-sonnet",
+      -- timeout = 30000, -- increase for reasoning models
+      -- temperature = 0,
+      max_tokens = 8192,
     },
   },
-  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = "make",
-  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
-    -- "stevearc/dressing.nvim",
+    "stevearc/dressing.nvim",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
-    --- The below dependencies are optional,
-    "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-    "zbirenbaum/copilot.lua", -- for providers='copilot'
+    "zbirenbaum/copilot.lua",
   },
 }
