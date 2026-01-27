d95c580e08b0c37241073b2e60b57a37e26e6a8d
Linus Boehm <linusboehm@gmail.com>
2026-01-27 04:10:45 +0000

    inlay hints pyrefly

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -122,3 +122,14 @@ vim.api.nvim_create_autocmd("FileType", {
     end, opts)
   end,
 })
+
+-- Enable inlay hints for Python files
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = augroup("lsp_inlay_hints"),
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client and client.name == "pyrefly" and client.server_capabilities.inlayHintProvider then
+      vim.lsp.inlay_hint.enable(true, { bufnr = args.buf })
+    end
+  end,
+})
