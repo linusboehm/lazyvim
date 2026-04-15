e37db436cc6f312fac518433253e63e85f1042f1
Linus Boehm <lboehm@core-ny4-dev8.eglp.com>
2026-04-15 14:05:33 +0000

    enable spell

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -11,6 +11,7 @@ opt.conceallevel = 1 -- Hide * markup for bold and italic
 opt.inccommand = "nosplit" -- preview incremental substitute
 opt.colorcolumn = "100" -- mark column
 opt.laststatus = 2 -- Show statusline per buffer
+opt.spell = true -- Enable spell checking by default
 
 vim.g.lazygit_config = false
 
